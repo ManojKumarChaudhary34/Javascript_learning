d4bdab10f8b2f7ae3de360a51899b795837b4c91
Manoj Kumar Chaudhary <106905644+ManojKumarChaudhary34@users.noreply.github.com>
2024-06-13 02:57:21 +0000

    memory concept added

---
+++ 01_basics/04_memory.js
@@ -0,0 +1,10 @@
+/*
+*********************memory******************
+
+    1. Stack -> all primitive data types uses Stack memory
+                last in first out(LIFO)
+
+    2. Heap -> all non-primitive data types uses heap memory
+
+    
+*/
