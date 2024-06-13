5b66de252989daa507dcfd9ea2ef46053b477436
Manoj Kumar Chaudhary <106905644+ManojKumarChaudhary34@users.noreply.github.com>
2024-06-13 02:06:15 +0000

    datatypes added

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,18 @@
+/*Data types refers to the types of values
+that we are storing or working with.
+
+******primitive data types******
+    1. Strings
+    2. Numbers
+    3. Boolean
+    4. null -> empty or unknown value, standalone value
+    5. undefined -> when you declare a varible but don't initialize it
+    6. BigInt -> large numbers
+    7. Symbol -> unique
+
+******non-primitive data types******
+    1. Object -> sub divided into 3 types
+        a. Object
+        b. Array
+        c. Date
+*/
