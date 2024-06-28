7ac5a3c03a46cfb2ad0b31c0257f1751d63612c7
Manoj Kumar Chaudhary <106905644+ManojKumarChaudhary34@users.noreply.github.com>
2024-06-28 02:38:48 +0000

    changes in memory.js

--- 01_basics/04_memory.js
+++ 01_basics/04_memory.js
@@ -3,8 +3,9 @@
 
     1. Stack -> all primitive data types uses Stack memory
                 last in first out(LIFO)
-
+                esme copy-value milta hai and original value change nahi hota hai
     2. Heap -> all non-primitive data types uses heap memory
+                esme reference milta hai and original value change hota hai
 
     
 */
