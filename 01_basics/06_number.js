b9d878b119915205ccc092f7df0bb4574857b7fb
Manoj Kumar Chaudhary <106905644+ManojKumarChaudhary34@users.noreply.github.com>
2024-07-02 00:37:01 +0000

    Math concept added

--- 01_basics/06_number.js
+++ 01_basics/06_number.js
@@ -17,3 +17,20 @@ console.log(number.toString().length); // output 3
 
 const otherNumber = new Number(123.678)
 console.log(otherNumber.toPrecision(3)); // output 124
+
+
+
+// ****************Math*********************
+
+
+console.log(typeof Math)
+console.log(Math.abs(-3)); //output is 3. the result is always positive or zero
+console.log(Math.round(245.65)); //output is 246
+console.log(Math.ceil(345.654)); //output is 346
+console.log(Math.floor(346.654)) //output is 346
+
+console.log(Math.random ); // returns a floating-point, pseudo-random number that's greater than or equal to 0 and less than 1
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min +1)) + min); //formula
